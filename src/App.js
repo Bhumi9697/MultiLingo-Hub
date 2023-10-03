734d5ee6275655ef9dbd5ac6a3735fee0efe9563
nimrahSohail <nimrah.sohail20@gmail.com>
2023-10-03 15:47:57 +0500

    Designing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import Translate from "./components/Translate";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Translate />
     </div>
   );
 }
